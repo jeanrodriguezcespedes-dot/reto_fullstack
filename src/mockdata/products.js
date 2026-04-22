29861bd618674c2c667514227bcdb1db1e4d25f2
Jean Christian Rodriguez Cespedes <jean.rodriguez.cespedes@gmail.com>
2026-04-21 22:49:10 -0500

    data: add product mockdata array

---
+++ src/mockdata/products.js
@@ -0,0 +1,19 @@
+// src/mockdata/products.js
+export const products = [
+  {
+    id: 1,
+    name: "Cámara Réflex Profesional",
+    price: 1200,
+    category: "Electrónica",
+    image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_.jpg",
+    description: "Cámara de alta resolución para fotografía profesional."
+  },
+  {
+    id: 2,
+    name: "Zapatillas Deportivas",
+    price: 85,
+    category: "Ropa",
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
+    description: "Comodidad y estilo para tus entrenamientos diarios."
+  }
+];
