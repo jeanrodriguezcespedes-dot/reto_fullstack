3767ef24e5f0d949555f33b4363dfa2629f933a6
Jean Christian Rodriguez Cespedes <jean.rodriguez.cespedes@gmail.com>
2026-04-22 22:32:34 -0500

    config: setup brand colors and fonts in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rougeRed: "#D32F2F", // Un rojo con fuerza pero profesional
+        rougeBlack: "#1A1A1A",
+      },
+      fontFamily: {
+        title: ["Playfair Display", "serif"],
+        body: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
